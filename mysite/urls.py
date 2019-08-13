68d0fcf771114cf21b7f809d0f33e2656f51e6b3
su-mmer <heekim987@gmail.com>
2019-08-13 11:08:39 +0900

    logout view edit

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/logout/', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
